a8841484c49e6e40c49af7c0eb675bcebc7618b8
Jeff Hutchinson <programmerjeff95@gmail.com>
2015-09-24 21:33:58 -0400

    show SSE 4 support

--- Engine/source/platformMac/osxCPUInfo.mm
+++ Engine/source/platformMac/osxCPUInfo.mm
@@ -262,6 +262,10 @@ void Processor::init()
 		Con::printf( "   SSE2 detected");
 	if (Platform::SystemInfo.processor.properties & CPU_PROP_SSE3)
 		Con::printf( "   SSE3 detected");
+	if (Platform::SystemInfo.processor.properties & CPU_PROP_SSE4_1)
+		Con::printf( "   SSE4.1 detected");
+	if (Platform::SystemInfo.processor.properties & CPU_PROP_SSE4_2)
+		Con::printf( "   SSE4.2 detected");
 	
 	Con::printf( "" );
    
